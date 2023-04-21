9d5d535563fb5cd35592d274cbf8e4bd666401e4
WojBor87 <w.borawski@cotynatostudio.pl>
2023-04-21 19:44:20 +0200

    UI Setup

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+from tkinter import *
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# ---------------------------- TIMER RESET ------------------------------- # 
 
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
+# ---------------------------- UI SETUP ------------------------------- #
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+window = Tk()
+window.title("The Pomodoro Technique")
+window.config(padx=100, pady=50, bg=YELLOW)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.grid(column=2, row=2)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+timer_label = Label(text="Timer", bg=YELLOW, font=(FONT_NAME, 45), fg=GREEN)
+timer_label.grid(column=2, row=1)
+check_label = Label(text="✔", bg=YELLOW, font=(FONT_NAME, 25), fg=GREEN)
+check_label.grid(column=2, row=4)
+start_button = Button(text="Start", bg=GREEN, highlightthickness=0)
+start_button.grid(column=1, row=3)
+reset_button = Button(text="Reset", bg=GREEN, highlightthickness=0)
+reset_button.grid(column=3, row=3)
+
+window.mainloop()
