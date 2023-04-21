259cc3a0d156e8862603dcfb1989192021a26ce5
WojBor87 <w.borawski@cotynatostudio.pl>
2023-04-21 21:08:12 +0200

    Timer Mechanism corrected + checked mechanism.

--- main.py
+++ main.py
@@ -9,26 +9,30 @@ FONT_NAME = "Courier"
 WORK_MIN = 20
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 25
-reps = 0
+REPS = 0
 
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    global reps
-    work_sec = WORK_MIN * 60
-    short_break_sec = SHORT_BREAK_MIN * 60
-    long_break_sec = LONG_BREAK_MIN * 60
-    if reps in [0, 2, 4, 6]:
+    global REPS
+    REPS = REPS % 7
+    work_sec = WORK_MIN
+    short_break_sec = SHORT_BREAK_MIN
+    long_break_sec = LONG_BREAK_MIN
+    if REPS in [0, 2, 4, 6]:
         count_down(work_sec)
-        reps += 1
-    elif reps in [1, 3, 5]:
+        timer_label.config(text="Work", fg=GREEN)
+        REPS += 1
+    elif REPS in [1, 3, 5]:
         count_down(short_break_sec)
-        reps += 1
-    elif reps == 7:
+        timer_label.config(text="Break", fg=PINK)
+        REPS += 1
+    elif REPS == 7:
         count_down(long_break_sec)
-        reps = 0
+        timer_label.config(text="Break", fg=RED)
+        REPS += 1
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -40,10 +44,13 @@ def count_down(count):
         window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        if REPS % 2 == 0:
+            check = REPS / 2
+            text = "✓" * int(check)
+            check_label.config(text=f"{text}")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
-
-
 window = Tk()
 window.title("The Pomodoro Technique")
 window.config(padx=100, pady=50, bg=YELLOW)
@@ -56,10 +63,13 @@ timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT
 
 timer_label = Label(text="Timer", bg=YELLOW, font=(FONT_NAME, 45), fg=GREEN)
 timer_label.grid(column=2, row=1)
-check_label = Label(text="✔", bg=YELLOW, font=(FONT_NAME, 25), fg=GREEN)
+
+check_label = Label(bg=YELLOW, font=(FONT_NAME, 25), fg=GREEN)
 check_label.grid(column=2, row=4)
+
 start_button = Button(text="Start", bg=GREEN, highlightthickness=0, command=start_timer)
 start_button.grid(column=1, row=3)
+
 reset_button = Button(text="Reset", bg=GREEN, highlightthickness=0)
 reset_button.grid(column=3, row=3)
 
