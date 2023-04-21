d7e7652d34c4938833433bdffa60d7f29b8167b3
WojBor87 <w.borawski@cotynatostudio.pl>
2023-04-21 20:03:37 +0200

    Working on timer mechanism

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,13 +10,23 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5 * 60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
-
+def count_down(count):
+    minutes = math.floor(count / 60)
+    seconds = count % 60
+    canvas.itemconfig(timer_text, text=f"{minutes:02}:{seconds:02}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title("The Pomodoro Technique")
@@ -25,13 +36,13 @@ canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.grid(column=2, row=2)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 
 timer_label = Label(text="Timer", bg=YELLOW, font=(FONT_NAME, 45), fg=GREEN)
 timer_label.grid(column=2, row=1)
 check_label = Label(text="✔", bg=YELLOW, font=(FONT_NAME, 25), fg=GREEN)
 check_label.grid(column=2, row=4)
-start_button = Button(text="Start", bg=GREEN, highlightthickness=0)
+start_button = Button(text="Start", bg=GREEN, highlightthickness=0, command=start_timer)
 start_button.grid(column=1, row=3)
 reset_button = Button(text="Reset", bg=GREEN, highlightthickness=0)
 reset_button.grid(column=3, row=3)
